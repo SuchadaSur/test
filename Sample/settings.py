9ca7014a0813b71b5994494b4ac6a046a46a04ab
NithipongR <nithipong.r@kubota.com>
2022-09-16 14:39:42 +0700

    Update settings.py

--- Sample/settings.py
+++ Sample/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'lk;akjdflkjsklfdjdaskl;fjdfalskdj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost','d701apsi01-la03skc.azurewebsites.net','skcone.siamkubota.co.th']
 
